8fe8dd5db7fd865f49435aabe1d66d53c3e2199d
Raphaël Goutmann <raphaelgoutmann@gmail.com>
2024-01-05 18:00:27 +0100

    Modifications qui seront validées : 	nouveau fichier : deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git add -A 
+git commit --no-edit 
+git push 
+@echo "You'll see the changes in your browser in a few minutes"
+@echo "Visit this link to find out more https://github.com/RaphaelGoutmann/Site-club-de-maths/actions/workflows/build.yml"
+
